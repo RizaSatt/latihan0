40225a6b632b393129747fb2d5b4caf33a190477
RizaSatt <satriaputra421@gmail.com>
2026-02-22 09:48:48 +0700

    membuat controller&method

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About {
+    public function index($nama='Budi', $pekerjaan = 'pengusaha')
+    {
+        echo "halo nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+
+    public function page ()
+    {
+        echo 'About/page';
+    }
+}
