454a61079e61489e1c07d2e9b495774dd49cf8bd
RizaSatt <satriaputra421@gmail.com>
2026-02-23 10:34:41 +0700

    menambahkan view

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">about me</h1>
+    <img src="<?=BASEURL ?> /img/profil.png" alt="" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan'];?></p>
+</div>
