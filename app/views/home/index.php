7d60413f865a40811cee855bfb9e17fcf0704d90
RizaSatt <satriaputra421@gmail.com>
2026-03-01 21:28:08 +0700

    coba1

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,6 +5,8 @@
   <p class="lead">Halo, nama saya <?= $data['nama']; ?>.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed eum praesentium error reiciendis nesciunt enim? Eos ipsa veniam corporis voluptas eius, veritatis sunt laborum. Odio sequi deleniti quam fugiat?</p>
   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  
 </div>
 </div>
